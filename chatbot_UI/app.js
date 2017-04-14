e3b8509217f9e86ec757cef77373b79ceb26fc92
Muyiwa Akin-Ogundeji <oakinogundeji@gmail.com>
2017-04-14 14:49:34 +0100

    :package: receiving welcome message on frontend

--- chatbot_UI/app.js
+++ chatbot_UI/app.js
@@ -8,6 +8,8 @@ require('dotenv').config();
 const
   express = require('express'),
   bParser = require('body-parser'),
+  ConversationV1 = require('watson-developer-cloud/conversation/v1'),
+  Promise = require('bluebird'),
   http = require('http'),
   path = require('path'),
   app = express(),
@@ -32,6 +34,60 @@ const
 if(ENV != 'production') {
   app.use(require('morgan')('dev'));
   require('clarify');
+}
+
+const ChatBot = new ConversationV1({
+  username: USERNAME,
+  password: PASSWORD,
+  path: {
+    workspace_id: WORKSPACE_ID
+  },
+  version_date: VERSION_DATE
+});
+//=============================================================================
+/**
+ * helper functions
+ */
+//=============================================================================
+function talkToBot(data) {
+  let
+    user_input,
+    bot_output;
+  if(!data) {
+    user_input = {};
+  } else {
+    user_input = {
+      input: {
+        text: data.trim()
+      }
+    };
+  }
+  return new Promise((resolve, reject) => {
+    return ChatBot.message(user_input, (err, resp) => {
+      if(err) {
+        console.error(err);
+        return reject(err);
+      }
+      if(resp.intents.length > 0) {
+        resp.intents.forEach(intent => {
+          console.log('detected intent: ');
+          console.log(intent);
+        });
+      }
+      if(resp.entities.length > 0) {
+        resp.entities.forEach(entity => {
+          console.log('detected entity: ');
+          console.log(entity);
+        });
+      }
+      if(resp.output.text.length != 0) {
+        console.log(resp);
+        console.log('\n');
+        console.log(resp.output.text[0] +' ...');
+        return resolve(resp.output.text[0]);
+      }
+    });
+  });
 }
 //=============================================================================
 /**
@@ -64,6 +120,12 @@ app.get('/', (req, res) => {
             return console.log('Index.html sent');
         }
     });
+});
+
+app.get('/welcome', (req, res) => {
+  return talkToBot(null)
+    .then(data => res.status(200).json({data: data}))
+    .catch(err => res.status(500).json({err: err}));
 });
 //=============================================================================
 /**
--- chatbot_UI/public/js/app.js
+++ chatbot_UI/public/js/app.js
@@ -16,7 +16,26 @@ Vue.http.options.root = '/root';
 const VM = new Vue({
   el: '#app',
   data: {
-    output: 'yaaay... it works!!!'
+    output: '',
+    userInput: '',
+    welcomeURL: '/welcome',
+    loadingSpinner: true
+  },
+  beforeMount: function () {
+    console.log('before mount fired...');
+    const URL = this.welcomeURL;
+    this.$http.get(URL)
+      .then(data => {
+        this.loadingSpinner = false;
+        console.log('response from backend...');
+        console.log(data);
+        return this.output = data.body.data;
+      })
+      .catch(info => {
+        this.loadingSpinner = false;
+        console.log('yawa gas...');
+        return console.log(info);
+      });
   }
 });
 //=============================================================================
