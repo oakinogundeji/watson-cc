fc845e39bf5c44c379eb96d751b4170d8cb0cd24
Muyiwa Akin-Ogundeji <oakinogundeji@gmail.com>
2017-04-14 08:29:29 +0100

    :package: completed phase 1 - connection to Watson Conversation Service successful

--- chatbot_CLI/app.js
+++ chatbot_CLI/app.js
@@ -1,6 +1,69 @@
 'use strict';
-function testApp() {
-    return console.log('All good!');
+require('dotenv').config();
+if(process.env.NODE_ENV != 'production') {
+  require('clarify');
 }
-
-setTimeout(testApp, 2000);
+//=============================================================================
+/**
+ * dependencies
+ */
+//=============================================================================
+const ConversationV1 = require('watson-developer-cloud/conversation/v1');
+//=============================================================================
+/**
+ * module variables
+ */
+//=============================================================================
+const
+  USERNAME = process.env.WATSON_USERNAME,
+  PASSWORD = process.env.WATSON_PASSWORD,
+  WORKSPACE_ID = process.env.WORKSPACE_ID,
+  VERSION_DATE = process.env.VERSION_DATE;
+//=============================================================================
+//=============================================================================
+/**
+ * helper functions
+ */
+//=============================================================================
+function responseHandler(err, resp) {
+  if(err) {
+    return console.error(err);
+  }
+  if(resp.intents.length > 0) {
+    resp.intents.forEach(intent => {
+      console.log('detected intent: ');
+      console.log(intent);
+    });
+  }
+  if(resp.entities.length > 0) {
+    resp.entities.forEach(entity => {
+      console.log('detected entity: ');
+      console.log(entity);
+    });
+  }
+  if(resp.output.text.length != 0) {
+    console.log(`response from Watson convo:`);
+    console.log(resp);
+    console.log(resp.output.text[0]);
+  }
+}
+//=============================================================================
+/**
+ * instantiate ChatBot
+ */
+//=============================================================================
+const ChatBot = new ConversationV1({
+  username: USERNAME,
+  password: PASSWORD,
+  path: {
+    workspace_id: WORKSPACE_ID
+  },
+  version_date: VERSION_DATE
+});
+//=============================================================================
+/**
+ * handle ChatBot
+ */
+//=============================================================================
+ChatBot.message({}, responseHandler);
+//=============================================================================
